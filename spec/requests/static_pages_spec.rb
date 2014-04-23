c05f0bcbc12c8b08b91f5e9e8e5290bba2ce72e0
Batname <dadubinin@gmail.com>
2014-04-24 00:48:26 +0300

    Home page stucture

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Twok Company'" do
       visit '/static_pages/home'
       expect(page).to have_content('Twok Company')
     end
