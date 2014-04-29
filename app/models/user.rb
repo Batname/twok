b8c684066d6e84c8147ef5cabaffc271695c062d
Batname <dadubinin@gmail.com>
2014-04-29 23:41:00 +0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
-  #attr_accessible :name, :email
-
+  before_save { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
